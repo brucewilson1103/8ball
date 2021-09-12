c0f9f955b3ddd05a1a1b7b95860f66e784039e77
da Vinci <brucewilson1103@gmail.com>
2021-09-12 15:20:40 -0400

    rework

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,24 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: class BallPage extends StatelessWidget {
-          const BallPage({Key? key}) : super(key: key);
-        
-          @override
-          Widget build(BuildContext context) {
-            return Scaffold(
-                appBar: AppBar(),
-                Container(),
-            );
-          }
-        }
-        ,
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: BallPage()
+    ),
+  );
+}
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
+        // title: Text('Solve your Dilemma'),
+        backgroundColor: Colors.deepPurple,
       ),
+      body: Container(),
     );
+  }
+}
